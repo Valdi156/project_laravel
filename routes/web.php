6b0edf6eac49bfa7619474f2cc19f03ef120c6cc
Valdi156 <vallxml9@gmail.com>
2025-02-05 15:11:20 +0700

    add : template

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,14 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+// 
+Route::get('/admin/inputbuku', function () {
+    return view('admin.inputbuku');
+});
+// 
+Route::get('/admin/kategori', function () {
+    return view('admin.kategori');
 });
